2dfaec987c31744d4f92ee0f1bd10747646e9ee6
Fred Blaise <madchap@users.noreply.github.com>
2021-03-23 11:27:14 +0100

    Update local_settings.py

--- docker/extra_settings/local_settings.py
+++ docker/extra_settings/local_settings.py
@@ -10,6 +10,12 @@ CELERY_BEAT_SCHEDULE['auto-delete-engagements'] = {
     'schedule': crontab(hour=9, minute=30)
 }
 
+CELERY_BEAT_SCHEDULE['jira_status_reconciliation'] = { 
+    'task': 'dojo.tasks.jira_status_reconciliation_task',
+    # 'schedule': crontab(hour=9, minute=30)
+    'kwargs': {'mode': 'import_status_from_jira', 'dryrun': True, 'daysback': 180, 'product': None, 'engagement': None}
+}
+
 # Override deduplication for certain parsers
 HASHCODE_FIELDS_PER_SCANNER['Anchore Engine Scan'] = ['title', 'severity', 'component_name', 'component_version', 'file_path']
 HASHCODE_ALLOWS_NULL_CWE['Anchore Engine Scan'] = True
